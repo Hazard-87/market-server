ff0e6d35d519586ead190a8736981e7a42b44b89
neo <dpetryshkin@gmail.com>
2023-07-31 14:25:34 +0300

    ашч

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
   })
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', '*')
     next()
   })
 

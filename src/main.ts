e73afa2764cec9d1dcd15c1a833f22944cbcfc84
neo <dpetryshkin@gmail.com>
2023-08-05 12:23:57 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import cookieParser from 'cookie-parser'
+// import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -18,7 +18,7 @@ async function bootstrap() {
   })
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
-  app.use(cookieParser())
+  // app.use(cookieParser())
 
   const config = new DocumentBuilder()
     .setTitle('Market')

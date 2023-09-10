181453c08fdd3955b8e26a614d43de77496d9bdc
neo <dpetryshkin@gmail.com>
2023-09-10 15:43:32 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api')
 
   app.enableCors({
-    origin: ['http://localhost:5173', '*'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:3000', '*'],
     allowedHeaders: ['Accept', 'Content-Type'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
